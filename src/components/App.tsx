3d5acae89408e4d61ab7a730301a7c7cd7562248
leandrod <leandro.e.dal.bo@gmail.com>
2024-11-22 18:42:03 +0000

    general css styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,6 @@ import Faqs from "../sections/faqs/Faqs";
 import FloatingNav from "../sections/floating-nav/FloatingNav";
 import Footer from "../sections/footer/Footer";
 import Header from "../sections/header/Header";
-import { Navbar } from "../sections/navbar/Navbar";
 import Portfolio from "../sections/portfolio/Portfolio";
 import Services from "../sections/services/Services";
 import Testimonials from "../sections/testimonials/Testimonials";
@@ -12,7 +11,6 @@ import Testimonials from "../sections/testimonials/Testimonials";
 function BugFree() {
   return (
     <main>
-      <Navbar />
       <Header />
       <About />
       <Services />
--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,7 +1,12 @@
+import { Navbar } from "../navbar/Navbar";
 import "./Header.css";
 
 const Header = () => {
-  return <section id="header">"Header"</section>;
+  return (
+    <section id="header">
+      <Navbar />
+    </section>
+  );
 };
 
 export default Header;
