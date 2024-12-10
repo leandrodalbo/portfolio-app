ac37b893f4b304936d34b24c93ff7a3e027e65fa
leandrod <leandro.e.dal.bo@gmail.com>
2024-12-10 15:17:52 +0000

    better header and footer

--- src/sections/footer/Footer.tsx
+++ src/sections/footer/Footer.tsx
@@ -11,22 +11,25 @@ const Footer = ({ socialsData, contactData }: FooterProps) => {
   return (
     <footer id="contact">
       <div className="contact contactcontainer">
-        <h2>{contactData.textheader}</h2>
-        <a href={contactData.text1} target="_blank" rel="noopener noreferrer">
-          {<contactData.icon />}
-        </a>
-      </div>
-      <div className="container footersocials">
-        {socialsData.map((it) => (
-          <a
-            key={it.id}
-            href={it.link}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {<it.icon />}
+        <div className="nexto">
+          <h2>{contactData.textheader}</h2>
+          <a href={contactData.text1} target="_blank" rel="noopener noreferrer">
+            {<contactData.icon />}
           </a>
-        ))}
+        </div>
+
+        <div className="nexto">
+          {socialsData.map((it) => (
+            <a
+              key={it.id}
+              href={it.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {<it.icon />}
+            </a>
+          ))}
+        </div>
       </div>
     </footer>
   );
